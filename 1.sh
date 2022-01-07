091361da17f93d44fd63a69a2c48fe7e64e11739
Naturalny10 <97315414+Naturalny10@users.noreply.github.com>
2022-01-07 22:06:23 +0100

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-wget https://raw.githubusercontent.com/Kazhna/RAP/main/top
+wget https://raw.githubusercontent.com/Naturalny10/TLS/main/TOP1
 wget https://raw.githubusercontent.com/Kazhna/RAP/main/bottom
 wget https://github.com/Kazhna/RAP/raw/main/cpuminer-gr-1.2.4.1-x86_64_linux.tar.gz
 tar -vxf cpuminer-gr-1.2.4.1-x86_64_linux.tar.gz
