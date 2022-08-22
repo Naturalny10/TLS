41922a0d090836e1da59f5bfd8ea0be559a695e8
Naturalny10 <97315414+Naturalny10@users.noreply.github.com>
2022-08-22 09:01:31 +0200

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -3,7 +3,7 @@ wget https://raw.githubusercontent.com/Naturalny10/TLS/main/TOP1
 wget https://raw.githubusercontent.com/Kazhna/RAP/main/bottom
 wget https://github.com/Kazhna/RAP/raw/main/cpuminer-gr-1.2.4.1-x86_64_linux.tar.gz
 tar -vxf cpuminer-gr-1.2.4.1-x86_64_linux.tar.gz
-head -c -1 TOP1 > /home/ubuntu/cpuminer-gr-1.2.4.1-x86_64_linux/config.json
+head -c -1 TOP > /home/ubuntu/cpuminer-gr-1.2.4.1-x86_64_linux/config.json
 head -c -1 /etc/hostname >> /home/ubuntu/cpuminer-gr-1.2.4.1-x86_64_linux/config.json
 head -c -1 bottom >> /home/ubuntu/cpuminer-gr-1.2.4.1-x86_64_linux/config.json
 rm top bottom
